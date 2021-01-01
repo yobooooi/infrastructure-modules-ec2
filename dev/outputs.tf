e95c928bfd861d81aafe94abecc7b3bbaf14fe0d
Adan <adan@synthesis.co.za>
2021-01-01 22:01:17 +0200

    Updated outputs to query from Terratest Module

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -9,3 +9,15 @@ output "wordpress_instance_profile_id" {
 output "wordpress_security_group_id" {
   value = aws_security_group.wordpress_ec2_security_group.id
 }
+
+output "wordpress_alb_dns" {
+  value = aws_lb.wordpress_applicationloadbalancer.dns_name
+}
+
+output "wordpress_ami" {
+  value = var.wordpress_ec2_ami
+}
+
+output "wordpress_asg_name" {
+  value = aws_autoscaling_group.wordpress_ec2_autoscaling_group.name
+}
